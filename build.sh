f29ed2f68a3db2b3abc8a9d3c5ef33d66d4ea85e
MathieuBALDO <mathieu.baldo.adm@gmail.com>
2021-06-26 23:31:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 set -x
 mydate=`date +%Y%m%d_%H%M%S`
-target_root_path="../pipeline/"
+target_root_path="/var/lib/jenkins/workspace/pipeline/"
 target_path=${target_root_path}${mydate}
 target_junction=${target_root_path}PROD
         
 pwd
-mkdir ${target_path}
+mkdir -p ${target_path}
 cp -r . ${target_path}
