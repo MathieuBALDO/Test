2a232b51e9d8cc201189e48dd5fb6cf56c6626dc
MathieuBALDO <mathieu.baldo.adm@gmail.com>
2021-06-26 22:41:45 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+mydate=`date +%Y%m%d_%H%M%S`
+target_root_path="/var/lib/jenkins/workspace/pipeline/"
+target_path=${target_root_path}/${mydate}
+target_junction=${target_root_path}/PROD
+        
+pwd
+mkdir ${target_path}
+cp -r . ${target_path}
