383facf4a650ae1792f664276fe09fedff81353d
MathieuBALDO <mathieu.baldo.adm@gmail.com>
2021-06-26 17:17:20 +0800

    Update callByJenkins.cmd

--- callByJenkins.cmd
+++ callByJenkins.cmd
@@ -1,2 +1,17 @@
 echo "Je suis dans mon script"
-mkdir C:\Users\Administrator\Pipeline\
+set mydate=%date:~10,4%%date:~4,2%%date:~7,2%
+set mytime=%time:~0,2%_%time:~3,2%_%time:~6,2%
+set mytime=%mytime: =0%
+set target_root_path="C:\Users\administrator\HOMEWARE\Pipeline\"
+set target_path=%target_root_path%\%mydate%_%mytime%
+set target_junction=%target_root_path%\PROD
+echo "Run trigger by GIT webhook %mydate% %mytime%
+echo %CD%
+
+echo "Create directory %my_target%"
+mkdir %target_path%
+
+xcopy /E . %target_path%
+
+rmdir %target_junction%
+mklink /J %target_junction% %target_path%
